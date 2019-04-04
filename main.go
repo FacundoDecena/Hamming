b255b2ebfc3809908fece3daf020d7ed77bae5a2
Facundo Decena <facundo.decena@gmail.com>
2019-04-04 00:40:07 -0300

    Tested xor and mask

--- main.go
+++ main.go
@@ -3,23 +3,27 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	//"strconv"
 	//"math/bits"
 )
 
 func main() {
 	var fileName string
+	maskFirst := 240
+	maskLast := 15
 
 	fmt.Println("Ingrese el nombre del archivo")
-	fmt.Scanf("%s", &fileName)
+	_, _ = fmt.Scanf("%s", &fileName)
 
 	body, err := loadFile(fileName)
 
 	if err != nil {
 		fmt.Println(err)
 	} else {
+		maskedFirst := body[0] & uint8(maskFirst)
+		maskedLast := body[0] & uint8(maskLast)
+		fmt.Printf("\nint: %08b masked first part: %08b masked last part: %08b", body[0], maskedFirst, maskedLast)
 		xor := body[0] ^ body[1]
-		fmt.Printf("%08b %08b %08b", body[0], body[1], xor)
+		fmt.Printf("\nbyte 1: %08b byte 2: %08b xor: %08b", body[0], body[1], xor)
 	}
 }
 
@@ -32,3 +36,9 @@ func loadFile(fileName string) ([]byte, error) {
 	}
 	return body, nil
 }
+
+/*func saveFile(fileName string, body []byte) error{
+
+	return ioutil.WriteFile(fileName, body, 0600)
+
+}*/
