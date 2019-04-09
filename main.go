b56cfddcc0dab89f29687d1a24e93c94d6e6f496
Facundo Decena <facundo.decena@gmail.com>
2019-04-09 00:29:40 -0300

    Added comments to easier maintenance

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 
 	fmt.Println("Ingrese el nombre del archivo")
 	_, _ = fmt.Scanf("%s", &fileName)
-
+	//Since golang does not show the time a program runs...
 	start := time.Now()
 
 	body, err := loadFile(fileName)
@@ -21,6 +21,7 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	} else {
+		//Start the timer
 		start = time.Now()
 		encodedBody = hamming7(body)
 
@@ -65,12 +66,14 @@ func hamming7(file []byte) []byte {
 			ret = append(ret, maskedFirst)
 			y = maskedLast
 		} else if j < 8 {
+			//now y is complete, then is saved in x and can be append to ret
 			x, y = compress(y, maskedFirst, j)
 			ret = append(ret, x)
-			x = y
+			//we can complete y with maskedLast first j bits, after that y meets the conditions to be x
 			x, y = compress(y, maskedLast, j)
 			ret = append(ret, x)
 		} else {
+			//Restart the process
 			j = 0
 		}
 		j++
@@ -79,14 +82,16 @@ func hamming7(file []byte) []byte {
 }
 
 func encode(bait byte) byte {
+	//Get bits from position in brackets and send it to the left
 	d4 := (bait & uint8(1)) >> 0
 	d3 := (bait & uint8(2)) >> 1
 	d2 := (bait & uint8(4)) >> 2
 	d1 := (bait & uint8(8)) >> 3
-
+	//Calculate controls using xor
 	c1 := d1 ^ d2 ^ d4
 	c2 := d1 ^ d3 ^ d4
 	c3 := d2 ^ d3 ^ d4
+	//set variables in their position
 	c2 = c2 << 1
 	d1 = d1 << 2
 	c3 = c3 << 3
@@ -97,14 +102,19 @@ func encode(bait byte) byte {
 	return d4 | d3 | d2 | c3 | d1 | c2 | c1
 }
 
+//Joins x and y, returns y moved to the left
 func compress(x byte, y byte, index int) (byte, byte) {
+	//My implementation for **
 	exp := 1
 	i := uint8(7 - index)
 	for ; i > 0; i-- {
 		exp *= 2
 	}
+	//Mask the bits we need
 	y1 := y & uint8(exp) >> uint8(7-index)
+	//Join x with the bits above
 	x = x | y1
+	//Move y index+1 places to the left
 	y = y << uint8(index+1)
 
 	return x, y
