c015de816e6bca2d81123b1e2a04d833d62cb993
Facundo Decena <facundo.decena@gmail.com>
2019-05-01 20:43:28 -0300

    Added option at menu for introduceErrors

--- introduceErrors.go
+++ introduceErrors.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func IntroduceErrors() {
@@ -15,16 +16,32 @@ func IntroduceErrors() {
 	clearScreen()
 	fmt.Println("Ingrese el nombre del archivo a introducir errores CON EXTENCION")
 	_, _ = fmt.Fscanf(r, "%s", &fileName)
+	//Clean buffer
+	_, _ = fmt.Fscanf(r, "%s")
 
 	body, err = loadFile(fileName)
 	if err != nil {
 		fmt.Println(err)
+		_, _ = fmt.Fscanf(r, "%s")
 		return
 	}
+	//Split the string between name and extention
+	extension := strings.Split(fileName, ".")
 
-	errorsFactory(body)
+	errorsFactory(body, extension[1])
 }
 
-func errorsFactory(file []byte) {
+func errorsFactory(file []byte, extension string) (fileWithErrors []byte) {
+	switch extension {
+	case "ha1":
+		fileWithErrors = insertError(file, 7)
+	case "ha2":
+	case "ha3":
+	case "ha4":
+	}
+	return file
+}
 
+func insertError(file []byte, kind int) []byte {
+	return file
 }
