a7e0f331c01bff9663486657618334ae0274ca8a
fefo <fede_16_98@hotmail.com>
2019-05-06 17:28:23 -0300

    function desHuffman

--- Huffman/DesHuffman.go
+++ Huffman/DesHuffman.go
@@ -6,6 +6,11 @@ import (
 	"os"
 )
 
+type Aux struct {
+	Caracter byte
+	Length   int
+}
+
 func callDeshuffman() {
 	var ret []byte
 
@@ -28,13 +33,14 @@ func callDeshuffman() {
 	}
 }
 
-func deshuffman(bodyCoded []byte, filename string) (originalBody []byte) {
+func deshuffman(bodyCoded []byte, fileName string) (originalBody [](byte)) {
 
-	diccionary := make(map[uint32]byte)
-	diccionary = stractTable("pruebahuffman.txt")
+	diccionary := make(map[uint32]Aux)
+	diccionary = stractTable(fileName)
 	var integer uint32
 	var result byte
 	var numberOfShift int
+	var length int
 
 	for index := 0; index < len(bodyCoded); index++ {
 		bait := bodyCoded[index]
@@ -45,13 +51,16 @@ func deshuffman(bodyCoded []byte, filename string) (originalBody []byte) {
 			valor := uint(8 * (4 - (numberOfShift)))
 			entero := uint32(baitAux)
 			integer |= entero << valor // 24 or 16 or 8 or 0
-			if diccionary[integer] != 0 {
-				// Element found.
-				result = diccionary[integer]
-				originalBody = append(originalBody, result)
-				integer = 0
-				count = 8
-				numberOfShift = 0
+			length++
+			if diccionary[integer].Length == length {
+				if diccionary[integer].Caracter != 0 {
+					// Element found.
+					result = diccionary[integer].Caracter
+					originalBody = append(originalBody, result)
+					integer = 0
+					length = 0
+					numberOfShift = 0
+				}
 			}
 		}
 
@@ -59,13 +68,11 @@ func deshuffman(bodyCoded []byte, filename string) (originalBody []byte) {
 	return originalBody
 
 }
-
-func stractTable(fileName string) map[uint32]byte {
+func stractTable(fileName string) map[uint32]Aux {
 	var body []byte
 	var arrByte []byte
-	var symbol byte
 	var code uint32
-	diccionary := make(map[uint32]byte)
+	diccionary := make(map[uint32]Aux)
 	var index int
 
 	body, err := loadFile(fileName)
@@ -73,8 +80,9 @@ func stractTable(fileName string) map[uint32]byte {
 		fmt.Println(err)
 	} else {
 		fmt.Printf("El texto de la tabla es : %c \n", body)
-		for index = 0; index < len(body); index += 5 {
-			symbol = body[index] // get the symbol
+		for index = 0; index < len(body); index += 6 {
+			var aux Aux
+			aux.Caracter = body[index] // get the symbol
 
 			// making de uint with the following 4 elements  after the first element of the array
 			arrByte = append(arrByte, body[index+1])
@@ -82,9 +90,10 @@ func stractTable(fileName string) map[uint32]byte {
 			arrByte = append(arrByte, body[index+3])
 			arrByte = append(arrByte, body[index+4])
 			code = (uint32(arrByte[0]) << 24) + (uint32(arrByte[1]) << 16) + (uint32(arrByte[2]) << 8) + uint32(arrByte[3])
-			fmt.Printf("%32b\n", code)
+			fmt.Printf("codificacion del elemento de la tabla %32b\n", code)
 			// making the table.
-			diccionary[code] = symbol
+			aux.Length = int(body[index+5])
+			diccionary[code] = aux
 			arrByte = nil
 		}
 
